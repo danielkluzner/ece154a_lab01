86a020aac9507f7ca9aeffbc1a7cb92d5c12c93e
Daniel Kluzner <danielkluzner@cs.ucsb.edu>
2019-10-01 23:14:45 -0700

    some files

---
+++ alu.v
@@ -0,0 +1,18 @@
+// alu.v
+// ECE 154A, Fall 2019
+// Authors: Daniel Kluzner, Benji
+
+// A 32-bit arithmetic logic unit (ALU)
+// as seen in 5.2.4 of "Harris & Harris"
+
+module alu(
+       input [31:0] a,
+       input [31:0] b,
+       input [2:0] f,
+       output [31:0] y,
+       output zero
+);
+
+// CODE
+
+endmodule
---
+++ testbench.v
@@ -0,0 +1,12 @@
+// testbench.v
+// ECE 154A, Fall 2019
+// Authors: Daniel Kluzner, Benji
+
+// A testbench for the "alu" module
+
+module alu_tb();
+
+// CODE
+
+endmodule
+
