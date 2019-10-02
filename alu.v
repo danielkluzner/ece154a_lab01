cc7256a4c3c853798bfc24153ffddb0cd1a9374a
Daniel Kluzner <danielkluzner@cs.ucsb.edu>
2019-10-02 16:23:32 -0700

    finished alu, wrote test vector file

--- alu.v
+++ alu.v
@@ -13,6 +13,39 @@ module alu(
        output zero
 );
 
-// CODE
+   reg temp0;
+   reg [31:0] tempy;
+   reg [31:0] tempb;   
+
+   always @* begin
+      
+      casex({f})
+	3'b000: tempy = a & b;
+	3'b001: tempy = a | b;
+	
+	// adder/subtractor case
+	3'bx10: begin
+	   if(f[2] == 0) tempb = b;
+	   else tempb = ~b;
+	   tempy = a + tempb + 32'h00000001;
+	end
+	/*
+	3'b010: tempy = a + b;
+	3'b110: tempy = a - b;
+	 */
+	3'b111: begin
+	   if(a[31] < b[31]) tempy = 32'h00000001;
+	   else tempy = 32'b0;
+	end
+	default: tempy = 32'b0;
+      endcase // case ({f})
+      
+      if(y == 8'h0) assign temp0 = 1'b1;
+      else assign temp0 = 1'b0;
+      
+   end
+
+   assign y = tempy;
+   assign zero = temp0;
 
 endmodule
--- testbench.v
+++ testbench.v
@@ -6,7 +6,35 @@
 
 module alu_tb();
 
-// CODE
+   reg [31:0] sim_a;
+   reg [31:0] sim_b;
+   reg [2:0]  sim_f;
+   wire [31:0] sim_y;
+   wire sim_zero;
+
+   alu a_test(
+	      .a(sim_a),
+	      .b(sim_b),
+	      .f(sim_f),
+	      .y(sim_y),
+	      .zero(sim_zero)
+	      );
+
+   initial begin
+
+      sim_a = 32'h00000003;
+      sim_b = 32'h0;
+      sim_f = 3'b0;
+
+      #5;
+
+      sim_b = 32'h00000001;
+
+      #5;
+
+      $stop;
+
+   end
 
 endmodule
 
