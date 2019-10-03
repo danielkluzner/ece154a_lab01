0fba80c56d68ec651b3856105c7216e6cfdc61e5
Daniel Kluzner <danielkluzner@cs.ucsb.edu>
2019-10-03 13:48:09 -0700

    almost done

--- testbench.v
+++ testbench.v
@@ -18,113 +18,41 @@ module alu_tb();
 	      .b(sim_b),
 	      .y(sim_y),
 	      .zero(sim_zero)
-	      );
+	      ); 
+	
+   //make memory array large enough to hold all the values from the input file (16 values), 
+   // //with each location large enough to hold the largest value (8 bits)
+   reg [31:0] data [0:104];   //16 memory words (lines), 8 bits wide
+   reg [2:0]  f_temp;
+   reg [31:0] a_temp, b_temp;
+   reg [31:0] y_temp;
+   reg zero_temp;
+   
+   integer i;
+   initial $readmemh("alu.tv", data);
 
    initial begin
+      for (i=0; i < 105; i=i+5) begin
+	 f_temp = data[i];
+	 a_temp = data[i+1];
+	 b_temp = data[i+2];
+	 y_temp = data[i+3];
+	 zero_temp = data[i+4];
+	 sim_a = a_temp;
+	 sim_b = b_temp;
+	 sim_f = f_temp;
 
-      sim_a = 32'b0;
-      sim_b = 32'b0;
-      sim_f = 3'b010;
+	 #5;
+	 if(y_temp == sim_y && zero_temp == sim_zero) begin
+	    //$display("sim_y: %h, sim_z: %d", sim_y, sim_zero);
+	    //$display("y_temp: %h, z_temp: %d", y_temp, zero_temp);
+	    $display("Test %d passed", i/5);
 
-      #5;
-
-      sim_b = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_a = 32'h00000001;
-
-      #5;
-
-      sim_a = 32'h000000FF;
-      sim_b = 32'h00000001;
-
-      #5;
-
-      sim_f = 3'b110;
-      sim_a = 32'b0;
-      sim_b = 32'b0;
-
-      #5;
-
-      sim_b = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_a = 32'h00000001;
-      sim_b = 32'h00000001;
-
-      #5;
-
-      sim_a = 32'h00000100;
-
-      #5;
-
-      sim_f = 3'b111;
-      sim_a = 32'b0;
-      sim_b = 32'b0;
-
-      #5;
-
-      sim_b = 32'h00000001;
-
-      #5;
-
-      sim_b = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_a = 32'h00000001;
-      sim_b = 32'b0;
-
-      #5;
-
-      sim_a = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_f = 3'b0;
-      sim_a = 32'hFFFFFFFF;
-      sim_b = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_b = 32'h12345678;
-
-      #5;
-
-      sim_a = 32'h12345678;
-      sim_b = 32'h87654321;
-
-      #5;
-
-      sim_a = 32'b0;
-      sim_b = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_f = 3'b001;
-      sim_a = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_a = 32'h12345678;
-      sim_b = 32'h87654321;
-
-      #5;
-
-      sim_a = 32'b0;
-      sim_b = 32'hFFFFFFFF;
-
-      #5;
-
-      sim_b = 32'b0;
-
-      #5;
+	 end
+	 #5;
+      end
 
       $stop;
-
-   end
+   end     
 
 endmodule
-
