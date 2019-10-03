ca0530bd967c34f3dfa9e2529aaff1bcce8422ff
Daniel Kluzner <danielkluzner@cs.ucsb.edu>
2019-10-02 19:57:52 -0700

    fixed alu, temp. testbench

--- alu.v
+++ alu.v
@@ -6,46 +6,22 @@
 // as seen in 5.2.4 of "Harris & Harris"
 
 module alu(
-       input [31:0] a,
-       input [31:0] b,
        input [2:0] f,
+       input [31:0]  a,
+       input [31:0]  b,
        output [31:0] y,
        output zero
 );
 
-   reg temp0;
-   reg [31:0] tempy;
-   reg [31:0] tempb;   
-
-   always @* begin
-      
-      casex({f})
-	3'b000: tempy = a & b;
-	3'b001: tempy = a | b;
-	
-	// adder/subtractor case
-	3'bx10: begin
-	   if(f[2] == 0) tempb = b;
-	   else tempb = ~b;
-	   tempy = a + tempb + 32'h00000001;
-	end
-	/*
-	3'b010: tempy = a + b;
-	3'b110: tempy = a - b;
-	 */
-	3'b111: begin
-	   if(a[31] < b[31]) tempy = 32'h00000001;
-	   else tempy = 32'b0;
-	end
-	default: tempy = 32'b0;
-      endcase // case ({f})
-      
-      if(y == 32'b0) assign temp0 = 1'b1;
-      else assign temp0 = 1'b0;
-      
-   end
-
-   assign y = tempy;
-   assign zero = temp0;
+   assign zero = (y == 32'b0) ? 1'b1 : 1'b0;
+   assign y = (f == 3'b000) ? (a & b) :
+	      (f == 3'b001) ? (a | b) :
+	      (f == 3'b010) ? (a + b) :
+	      (f == 3'b110) ? (a - b) :
+	      (f == 3'b111 && a[31] < b[31]) ? (32'b0) :
+	      (f == 3'b111 && a[31] > b[31]) ? (32'h00000001) :
+	      (f == 3'b111 && a[30:0] < b[30:0]) ? (32'h00000001) :
+	      (f == 3'b111 && a[30:0] > b[30:0]) ? (32'b0) :
+	      32'b0;
 
 endmodule
--- testbench.v
+++ testbench.v
@@ -6,29 +6,119 @@
 
 module alu_tb();
 
+   reg [2:0]  sim_f;
    reg [31:0] sim_a;
    reg [31:0] sim_b;
-   reg [2:0]  sim_f;
    wire [31:0] sim_y;
    wire sim_zero;
 
    alu a_test(
+	      .f(sim_f),
 	      .a(sim_a),
 	      .b(sim_b),
-	      .f(sim_f),
 	      .y(sim_y),
 	      .zero(sim_zero)
 	      );
 
    initial begin
 
-      sim_a = 32'h00000003;
-      sim_b = 32'h0;
-      sim_f = 3'b0;
+      sim_a = 32'b0;
+      sim_b = 32'b0;
+      sim_f = 3'b010;
+
+      #5;
+
+      sim_b = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_a = 32'h00000001;
+
+      #5;
+
+      sim_a = 32'h000000FF;
+      sim_b = 32'h00000001;
+
+      #5;
+
+      sim_f = 3'b110;
+      sim_a = 32'b0;
+      sim_b = 32'b0;
+
+      #5;
+
+      sim_b = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_a = 32'h00000001;
+      sim_b = 32'h00000001;
+
+      #5;
+
+      sim_a = 32'h00000100;
+
+      #5;
+
+      sim_f = 3'b111;
+      sim_a = 32'b0;
+      sim_b = 32'b0;
 
       #5;
 
       sim_b = 32'h00000001;
+
+      #5;
+
+      sim_b = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_a = 32'h00000001;
+      sim_b = 32'b0;
+
+      #5;
+
+      sim_a = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_f = 3'b0;
+      sim_a = 32'hFFFFFFFF;
+      sim_b = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_b = 32'h12345678;
+
+      #5;
+
+      sim_a = 32'h12345678;
+      sim_b = 32'h87654321;
+
+      #5;
+
+      sim_a = 32'b0;
+      sim_b = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_f = 3'b001;
+      sim_a = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_a = 32'h12345678;
+      sim_b = 32'h87654321;
+
+      #5;
+
+      sim_a = 32'b0;
+      sim_b = 32'hFFFFFFFF;
+
+      #5;
+
+      sim_b = 32'b0;
 
       #5;
 
